3d209e6a8f77f14e9095c17904c65910601783b4
santimg92 <santiago.mg92@hotmail.com>
2022-09-04 21:41:30 -0300

    mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+const sumar = require("./sumar")
+const restar = require("./restar")
+const multiplicar = require("./multiplicar")
+const dividir = require("./dividir")
+
+console.log(sumar(5,4))
+console.log(restar(6,3))
+console.log(multiplicar(2,2))
+console.log(dividir(9,3))
+console.log(dividir(0,3))
+
